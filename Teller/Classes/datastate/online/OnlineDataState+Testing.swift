ea7ca97ac7689c065c69259b091fab650a2d31b6
Levi Bostian <levi.bostian@gmail.com>
2019-09-18 08:06:39 -0500

    Create instances of OnlineDataState for testing

---
+++ Teller/Classes/datastate/online/OnlineDataState+Testing.swift
@@ -0,0 +1,30 @@
+//
+//  OnlineDataState+Testing.swift
+//  Teller
+//
+//  Created by Levi Bostian on 9/18/19.
+//
+
+import Foundation
+
+public extension OnlineDataState {
+
+    static var testing: Testing<DataType> {
+        return Testing()
+    }
+
+    class Testing<DataType: Any> {
+        public func none() -> OnlineDataState<DataType> {
+            return OnlineDataStateTesting.none()
+        }
+
+        public func noCache(requirements: OnlineRepositoryGetDataRequirements, more: ((inout NoCacheExistsDsl) -> Void)? = nil) -> OnlineDataState<DataType> {
+            return OnlineDataStateTesting.noCache(requirements: requirements, more: more)
+        }
+
+        public func cache(requirements: OnlineRepositoryGetDataRequirements, lastTimeFetched: Date, more: ((inout CacheExistsDsl<DataType>) -> Void)? = nil) -> OnlineDataState<DataType> {
+            return OnlineDataStateTesting.cache(requirements: requirements, lastTimeFetched: lastTimeFetched, more: more)
+        }
+    }
+
+}
---
+++ Teller/Classes/testing/cachestate/OnlineDataStateTesting.swift
@@ -0,0 +1,158 @@
+//
+//  OnlineCacheStateTesting.swift
+//  Teller
+//
+//  Created by Levi Bostian on 9/17/19.
+//
+
+import Foundation
+
+public class OnlineDataStateTesting {
+
+    private init() {
+    }
+
+    static public func none<DataType: Any>() -> OnlineDataState<DataType> {
+        return OnlineDataState.none()
+    }
+
+    static public func noCache<DataType: Any>(requirements: OnlineRepositoryGetDataRequirements, more: ((inout NoCacheExistsDsl) -> Void)? = nil) -> OnlineDataState<DataType> {
+        var noCacheExists = NoCacheExistsDsl()
+
+        if let more = more {
+            more(&noCacheExists)
+        }
+
+        /**
+         * We are using the [OnlineCacheStateStateMachine] here to (1) prevent duplicate constructor code that is a pain to maintain and (2) we are starting with the assumption that no cache exists and editing the state from there if the DSL asks for it.
+         */
+        var stateMachine = OnlineDataStateStateMachine<DataType>.noCacheExists(requirements: requirements)
+
+        if noCacheExists.props.fetchingFirstTime {
+            stateMachine = try! stateMachine.change().firstFetch()
+        }
+
+        if let errorDuringFirstFetch = noCacheExists.props.errorDuringFirstFetch {
+            stateMachine = try! stateMachine.change()
+                .firstFetch().change()
+                .errorFirstFetch(error: errorDuringFirstFetch)
+        }
+
+        if noCacheExists.props.successfulFirstFetch {
+            stateMachine = try! stateMachine.change()
+            .firstFetch().change()
+            .successfulFirstFetch(timeFetched: noCacheExists.props.timeFetched!)
+        }
+
+        return stateMachine
+    }
+
+    static public func cache<DataType: Any>(requirements: OnlineRepositoryGetDataRequirements, lastTimeFetched: Date, more: ((inout CacheExistsDsl<DataType>) -> Void)? = nil) -> OnlineDataState<DataType> {
+        var cacheExists = CacheExistsDsl<DataType>()
+
+        if let more = more {
+            more(&cacheExists)
+        }
+
+        var stateMachine = OnlineDataStateStateMachine<DataType>.cacheExists(requirements: requirements, lastTimeFetched: lastTimeFetched)
+
+        if let cache = cacheExists.props.cache {
+            stateMachine = try! stateMachine.change()
+                .cachedData(cache)
+        } else {
+            stateMachine = try! stateMachine.change()
+                .cacheIsEmpty()
+        }
+
+        if cacheExists.props.fetching {
+            stateMachine = try! stateMachine.change().fetchingFreshCache()
+        }
+
+        if let fetchError = cacheExists.props.errorDuringFetch {
+            stateMachine = try! stateMachine.change()
+            .fetchingFreshCache().change()
+            .failFetchingFreshCache(fetchError)
+        }
+
+        if cacheExists.props.successfulFetch {
+            stateMachine = try! stateMachine.change()
+            .fetchingFreshCache().change()
+            .successfulFetchingFreshCache(timeFetched: cacheExists.props.successfulFetchTime!)
+        }
+
+        return stateMachine
+    }
+
+}
+
+public class NoCacheExistsDsl {
+    internal var props = Props()
+
+    internal init() {
+    }
+
+    public func fetchingFirstTime() {
+        props = Props()
+        props.fetchingFirstTime = true
+    }
+
+    public func failedFirstFetch(error: Error) {
+        props = Props()
+        props.errorDuringFirstFetch = error
+    }
+
+    public func successfulFirstFetch(timeFetched: Date) {
+        props = Props()
+        props.successfulFirstFetch = true
+        props.timeFetched = timeFetched
+    }
+
+    internal struct Props {
+        var fetchingFirstTime: Bool = false
+        var errorDuringFirstFetch: Error? = nil
+        var successfulFirstFetch: Bool = false
+        var timeFetched: Date? = nil
+    }
+}
+
+public class CacheExistsDsl<DataType: Any> {
+    internal var props = Props<DataType>()
+
+    public func cache(cache: DataType) {
+        props = Props()
+        props.cache = cache
+    }
+
+    public func fetching() {
+        let oldCache = self.props.cache
+
+        props = Props()
+        props.cache = oldCache
+        props.fetching = true
+    }
+
+    public func failedFetch(error: Error) {
+        let oldCache = self.props.cache
+
+        props = Props()
+        props.cache = oldCache
+        props.errorDuringFetch = error
+    }
+
+    public func successfulFetch(timeFetched: Date) {
+        let oldCache = self.props.cache
+
+        props = Props()
+        props.cache = oldCache
+        props.successfulFetch = true
+        props.successfulFetchTime = timeFetched
+    }
+
+    internal struct Props<DataType: Any> {
+        var cache: DataType? = nil
+        var fetching: Bool = false
+        var errorDuringFetch: Error? = nil
+        var successfulFetch: Bool = false
+        var successfulFetchTime: Date? = nil
+    }
+}
